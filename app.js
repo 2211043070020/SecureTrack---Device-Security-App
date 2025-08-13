ac3a0d558c78ecdf35872c2b8c8d21ba4eeb250d
SAIISARRAN <147138065+2211043070020@users.noreply.github.com>
2025-08-13 17:57:21 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DeviceTrackerApp from './components/DeviceTrackerApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DeviceTrackerApp />
+    </div>
+  );
+}
+
+export default App;
